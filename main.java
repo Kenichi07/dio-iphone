ef0fd7429e442876ed99944f72cd4a5c2aae5f40
Vinicius Huang <viniciushuangbr@gmail.com>
2024-05-26 17:22:34 -0300

    Segundo Commit

--- main.java
+++ main.java
@@ -2,5 +2,13 @@ public class main {
   public static void main(String[] args) {
       Iphone iphone = new Iphone();
       iphone.ligar("99999-9999");
-  }
+      iphone.atender();
+      iphone.iniciarCorreioVoz();
+      iphone.exibirPagina("www.youtube.com");
+      iphone.adicionarNovaAba();
+      iphone.atualizarPagina();
+      iphone.tocar();
+      iphone.pausar();
+      iphone.selecionarMusica("FADED");
+    }
 }
